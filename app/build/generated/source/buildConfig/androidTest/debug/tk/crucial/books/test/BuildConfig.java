c4525c7e2f6e90e094b9ee8b919143258d5ea5ab
Nicholas <nich.otieno@gmail.com>
2019-06-14 18:23:45 +0300

    Initial commit

---
+++ app/build/generated/source/buildConfig/androidTest/debug/tk/crucial/books/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package tk.crucial.books.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "tk.crucial.books.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
